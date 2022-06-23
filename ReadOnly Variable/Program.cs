ccef60de3c568c01ff431d8248151df22c8f5dcd
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-24 03:44:04 +0530

    Add project files.

---
+++ ReadOnly Variable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReadOnly_Variable
+{
+    class Program
+    {
+        readonly int pei;
+
+        Program(int num)
+        {
+            this.pei = num;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Program obj = new Program(10);
+            Console.WriteLine("Readonly variable :" + obj.pei);
+
+            Program obj1 = new Program(100);
+            Console.WriteLine("Readonly variable :" + obj1.pei);
+
+            Program obj2 = new Program(1000);
+            Console.WriteLine("Readonly variable :" + obj2.pei);
+        }
+    }
+}
